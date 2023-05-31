0971ef64481071410c7ce277d18305d6f190bad5
Joshua Piech <joshuapiech@gmail.com>
2023-05-31 20:13:03 +0200

    NO-TICKET:  - added phpunit  - wip

---
+++ tests/MathServiceTest.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Tests;
+
+use PHPUnit\Framework\TestCase;
+
+class MathServiceTest extends TestCase
+{
+
+    public function testFibonacciArrayIsCorrect(): void
+    {
+        $this->assertTrue(true);
+    }
+
+    public function testSomething(): void
+    {
+        $this->assertTrue(true);
+    }
+}
